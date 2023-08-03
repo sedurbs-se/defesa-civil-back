40c6666961f3ce0dc1a1ef194804cf40efa44547
Thiago Freire <thiago.freire45@gmail.com>
2023-08-03 15:56:21 -0300

    fix: disable in memory repository

--- src/modules/disasters/repositories/in-memory/InMemoryAffectedAreaRepository.ts
+++ src/modules/disasters/repositories/in-memory/InMemoryAffectedAreaRepository.ts
@@ -1,68 +1,72 @@
-import { PrismaService } from 'src/prisma.service';
-import { AffectedAreaRepository } from '../IAffectedAreaRepository';
-import { Injectable } from '@nestjs/common';
-import { AffectedArea } from '../../domain/affectedArea/affected-area';
-import { AffectedAreaMapper } from '../../mappers/AffectedAreaMapper';
+// import { PrismaService } from 'src/prisma.service';
+// import { AffectedAreaRepository } from '../IAffectedAreaRepository';
+// import { Injectable } from '@nestjs/common';
+// import { AffectedArea } from '../../domain/affectedArea/affected-area';
+// import { AffectedAreaMapper } from '../../mappers/AffectedAreaMapper';
 
 
-export class InMemoryAffectedAreaRepository implements AffectedAreaRepository {
-  constructor(readonly prisma: PrismaService) {}
+// export class InMemoryAffectedAreaRepository implements AffectedAreaRepository {
+//   constructor(private db: AffectedArea[] = []) {};
 
-  async save(affectedArea: AffectedArea): Promise<void> {
-    const a = AffectedAreaMapper.toPersistence(affectedArea);
+//   async save(affectedArea: AffectedArea): Promise<void> {
+//     const a = AffectedAreaMapper.toPersistence(affectedArea);
 
-    await this.prisma.areaAfetada.create({
-      data: {
-        ...a,
-      },
-    });
-  }
+//     await this.db.push()
 
-  async update(affectedArea: AffectedArea): Promise<void> {
-    const a = AffectedAreaMapper.toPersistence(affectedArea);
+//     await this.prisma.areaAfetada.create({
+//       data: {
+//         ...a,
+//       },
+//     });
+//   }
 
-    await this.prisma.areaAfetada.update({
-      where: {
-        id: a.id,
-      },
-      data: {
-        ...a,
-      },
-    });
-  }
+//   async update(affectedArea: AffectedArea): Promise<void> {
+//     const a = AffectedAreaMapper.toPersistence(affectedArea);
 
-  async find(id: string): Promise<AffectedArea> {
-    const area = await this.prisma.areaAfetada.findUnique({
-      where: {
-        id,
-      },
-      include: {
-        unidadesHabitacionais: true,
-      },
-    });
 
-    if (!area) return null;
 
-    return AffectedAreaMapper.toDomain(area);
-  }
-  async findByOrder(order: number): Promise<AffectedArea> {
-    const area = await this.prisma.areaAfetada.findUnique({
-      where: {
-        ORDEM: order,
-      },
-      include: {
-        unidadesHabitacionais: true,
-      },
-    });
+//     await this.prisma.areaAfetada.update({
+//       where: {
+//         id: a.id,
+//       },
+//       data: {
+//         ...a,
+//       },
+//     });
+//   }
 
-    if (!area) return null;
+//   async find(id: string): Promise<AffectedArea> {
+//     const area = await this.prisma.areaAfetada.findUnique({
+//       where: {
+//         id,
+//       },
+//       include: {
+//         unidadesHabitacionais: true,
+//       },
+//     });
 
-    return AffectedAreaMapper.toDomain(area);
-  }
+//     if (!area) return null;
 
-  async findAll(): Promise<AffectedArea[]> {
-    const areas = await this.prisma.areaAfetada.findMany({});
+//     return AffectedAreaMapper.toDomain(area);
+//   }
+//   async findByOrder(order: number): Promise<AffectedArea> {
+//     const area = await this.prisma.areaAfetada.findUnique({
+//       where: {
+//         ORDEM: order,
+//       },
+//       include: {
+//         unidadesHabitacionais: true,
+//       },
+//     });
 
-    return areas.map(AffectedAreaMapper.toDomain);
-  }
-}
+//     if (!area) return null;
+
+//     return AffectedAreaMapper.toDomain(area);
+//   }
+
+//   async findAll(): Promise<AffectedArea[]> {
+//     const areas = await this.prisma.areaAfetada.findMany({});
+
+//     return areas.map(AffectedAreaMapper.toDomain);
+//   }
+// }
