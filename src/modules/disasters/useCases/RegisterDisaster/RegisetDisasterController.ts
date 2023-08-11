2d1cd7ed4dff3c62df6bfedee8161aabd3da801a
Paulo Hercilio Araujo Santana <phasantana@sedurbs.se>
2023-08-11 10:40:47 -0300

    mend

--- src/modules/disasters/useCases/RegisterDisaster/RegisetDisasterController.ts
+++ src/modules/disasters/useCases/RegisterDisaster/RegisetDisasterController.ts
@@ -1,14 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { RegisterDisaster } from './RegisterDisaster';
 import { CreateDisasterDTO } from '../../dtos/CreateDisasterDTO';
-import { Disaster } from '../../domain/disaster/disaster';
 
 @Controller()
 export class RegisterDisasterController {
   constructor(private readonly registerDisaster: RegisterDisaster) {}
 
   @Post('/disaster')
-  async execute(@Body() body: CreateDisasterDTO): Promise<Disaster> {
-    return await this.registerDisaster.execute(body);
+  async execute(@Body() body: CreateDisasterDTO) {
+    const d = await this.registerDisaster.execute(body);
+
+    return {
+      id: d.id,
+      date: d.date,
+      cityId: d.cityId,
+    };
   }
 }
