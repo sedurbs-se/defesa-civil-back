165ae8a75bf16470e4c606d89f8f717754bdf8b1
Thiago Freire <thiago.freire45@gmail.com>
2023-08-30 11:12:43 -0300

    feat: edit agent

---
+++ src/modules/disasters/useCases/EditAgent/EditAgentController.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
+import { EditAgent } from './EditAgent';
+import { EditAgentDTO } from '../../dtos/EditAgentDTO';
+import { Agent } from '../../domain/agent/agent';
+
+@Controller()
+export class EditAgentController {
+  constructor(private editAgent: EditAgent) {}
+
+  @Put('/agent/:id')
+  async handle(@Param('id') id: string, @Body() body: EditAgentDTO) {
+    const agent = new Agent(body);
+    await this.editAgent.execute({ id, agent });
+  }
+}
